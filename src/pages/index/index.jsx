2cb06e01a35cfc006d58f36e9001dbb2b7c03800
FelixStevian11 <felixstevian00@gmail.com>
2024-07-04 14:33:03 +0800

    add navbar 0.1

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,7 +1,9 @@
+import NavBar from '../../components/navbar/index'
+
 const index = () => {
     return(
         <div>
-            Hello world
+            <NavBar></NavBar>
         </div>
     )
 };
