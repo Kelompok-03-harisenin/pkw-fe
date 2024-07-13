01f6dc9d0ec9ce3a717421de3fda3f3f6fc54078
aufaizza <aufashidqiizza@gmail.com>
2024-07-13 11:11:05 +0700

    added editing profile but stuck in uploading photo

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -1,19 +1,117 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
 import Image1 from '../../assets/layer-2.svg'
+import Navbar from '../../components/navbar/index'
+import UseAuthStore from '../../store/authStore.js'
+import { useParams } from 'react-router-dom';
+import axios from "axios"
 
 const ProfilePage = () => {
+  const { userID } = useParams()
   const [isEditing, setIsEditing] = useState(false);
   const [profileData, setProfileData] = useState({
-    name: 'Angelina Stoichkov',
-    title: 'Photographer',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    photo: '',
+    name: '',
+    title: '',
+    biography: ''
   });
+  const jwt = UseAuthStore((state) => state.jwt);
+  const [apiData, setApiData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isCurrentUser, setIsCurrentUser] = useState(false)
+  const [photo, setPhoto] = useState(null)
+
+  useEffect(() => {
+    if (jwt == null) {
+      setIsLoggedIn(false)
+    } else {
+      setIsLoggedIn(true)
+    }
+  }, [jwt, userID])
+
+  useEffect(() => {
+    if (isLoggedIn == false) {
+      return
+    }
+
+    axios.get(`http://localhost:3000/api/users/token`, {
+      headers: {
+        Authorization: `Bearer ${jwt}`
+      }
+    }).then((res) => {
+      if (res.status == 200) {
+        const data = res.data.data
+
+        if (data.id == userID) {
+          setIsCurrentUser(true)
+        } else {
+          setIsCurrentUser(false)
+        }
+      }
+    })
+  }, [isLoggedIn, userID])
+
+  useEffect(() => {
+    axios.get(`http://localhost:3000/api/users/${userID}`, {
+    }).then((res) => {
+      if (res.status == 200) {
+        setApiData(res.data.data)
+        const data = res.data.data
+        setProfileData({
+          photo: data.profile_picture,
+          name: data.name,
+          title: data.title,
+          biography: data.biography
+        })
+        setIsLoading(false)
+      }
+    })
+  }, [isLoading, userID])
+
+  /**
+  * @param {React.ChangeEvent<HTMLInputElement>} e
+  */
+  const handleFileChange = (e) => {
+    if (e.target.files) {
+      setPhoto(e.target.files[0])
+    }
+  }
 
   const handleEdit = () => {
     setIsEditing(true);
   };
 
   const handleSave = () => {
+    if (photo) {
+      let formData = new FormData();
+      formData.append('image', photo)
+      console.log(photo)
+
+      axios.patch(`http://localhost:3000/api/users/${userID}`, {
+        image: photo
+      }, {
+        headers: {
+          Authorization: `Bearer ${jwt}`
+        }
+      }).then(res => {
+        setIsLoading(true)
+      })
+    }
+
+    axios.patch(`http://localhost:3000/api/users/${userID}`, {
+      name: profileData.name,
+      biography: profileData.biography,
+      title: profileData.title,
+    }, {
+      headers: {
+        Authorization: `Bearer ${jwt}`
+      }
+    }).then(res => {
+      setIsLoading(true)
+    })
+
+
     setIsEditing(false);
   };
 
@@ -22,40 +120,48 @@ const ProfilePage = () => {
     setProfileData(prev => ({ ...prev, [name]: value }));
   };
 
+  if (isLoading == true) {
+    return (
+      <div></div>
+    )
+  }
+
   return (
     <div className="bg-gray-100 min-h-screen">
-      
-      <section className="max-w-3xl mx-auto pt-8 bg-white rounded-lg shadow-md overflow-hidden">
+      <Navbar></Navbar>
+
+      <section className="max-w-3xl mx-auto pb-8 pt-8 bg-white rounded-lg shadow-md overflow-hidden">
         {isEditing ? (
           <div className="flex flex-col h-40 bg-cover bg-center justify-center items-center gap-4 pt-10">
-              <img
-                src={Image1}
-                alt="Profile"
-                className="bottom-0 left-1/2 transform w-32 h-32 rounded-full border-4 border-white object-cover"
+            <img
+              src={profileData.photo}
+              alt="Profile"
+              className="bottom-0 left-1/2 transform w-32 h-32 rounded-full border-4 border-white object-cover"
+            />
+            <div className="flex justify-center flex-col">
+              <input
+                type="file"
+                className=" border-solid border-2 p-3 rounded-lg"
+                accept="image/jpeg, image/jpg, image/png"
+                onChange={handleFileChange}
               />
-              <div className="flex justify-center flex-col">
-                <input 
-                  type="file" 
-                  className=" border-solid border-2 p-3 rounded-lg"
-                  accept="image/jpeg, image/jpg, image/png"
-                />
-                <p className="font-light text-xs text-gray-500">Accepted file types : JPEG, JPG, PNG</p>
+              <p className="font-light text-xs text-gray-500">Accepted file types : JPEG, JPG, PNG</p>
 
-              </div>
+            </div>
 
           </div>
-        ) : 
-        (
-        <div className="relative h-40 bg-cover bg-center">
-          <img
-            src={Image1}
-            alt="Profile"
-            className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 w-32 h-32 rounded-full border-4 border-white object-cover"
-          />
-        </div>)
+        ) :
+          (
+            <div className="relative h-40 bg-cover bg-center">
+              <img
+                src={profileData.photo}
+                alt="Profile"
+                className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 w-32 h-32 rounded-full border-4 border-white object-cover"
+              />
+            </div>)
 
         }
-        
+
         <div className="mt-16 p-6 text-center">
           {isEditing ? (
             <>
@@ -74,8 +180,8 @@ const ProfilePage = () => {
                 className="text-gray-600 mb-4 text-center"
               />
               <textarea
-                name="description"
-                value={profileData.description}
+                name="biography"
+                value={profileData.biography}
                 onChange={handleChange}
                 className="w-full p-2 border rounded"
                 rows="4"
@@ -88,23 +194,29 @@ const ProfilePage = () => {
             <>
               <h1 className="text-2xl font-bold mb-2">{profileData.name}</h1>
               <p className="text-gray-600 mb-4">{profileData.title}</p>
-              <p className="text-sm text-gray-700 mb-4">{profileData.description}</p>
-              <button onClick={handleEdit} className="px-4 py-2 border rounded mr-2">
-                Edit profile
-              </button>
-              <button className="px-4 py-2 bg-red-600 text-white rounded">
-            Upload Photo
-          </button>
+              <p className="text-sm text-gray-700 mb-4">{profileData.biography}</p>
+              {
+                isCurrentUser ? (
+                  <div>
+                    <button onClick={handleEdit} className="px-4 py-2 border rounded mr-2">
+                      Edit profile
+                    </button>
+                    <button className="px-4 py-2 bg-red-600 text-white rounded">
+                      Upload Photo
+                    </button>
+                  </div>
+                ) : (<div></div>)
+              }
             </>
           )}
-          
+
         </div>
       </section>
 
-      
+
       <section className="max-w-7xl mx-auto mt-8 px-4 pb-8">
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
-         
+
           {Array.from({ length: 20 }).map((_, index) => (
             <div key={index} className="aspect-square bg-gray-200 rounded-md overflow-hidden">
               <img
